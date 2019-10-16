a96710da8ceaa545ffbf87c6f952823cbcfcc3a9
YanceyOffical <yanceyofficial@gmail.com>
2019-10-16 17:15:32 +0800

    :sparkles: feat: supprt security middlewares

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,9 +2,21 @@ import { INestApplication } from '@nestjs/common'
 import path from 'path'
 import serveFavicon from 'serve-favicon'
 import bodyParser from 'body-parser'
+import helmet from 'helmet'
+import csurf from 'csurf'
+import rateLimit from 'express-rate-limit'
 
 export const configMiddlewares = (app: INestApplication) => {
   app.use(serveFavicon(path.join(process.cwd(), 'public/assets/favicon.ico')))
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(helmet())
+  app.use(csurf())
+  app.enableCors({})
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  )
 }
