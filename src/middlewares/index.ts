5bff97098ce6e8c814b530eed1aa9d60a05dbd15
YanceyOffical <yanceyofficial@gmail.com>
2019-10-16 16:02:56 +0800

    :heavy_minus_sign: dep_rm: delete useless dependencies

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,12 +1,10 @@
 import { INestApplication } from '@nestjs/common'
 import path from 'path'
 import serveFavicon from 'serve-favicon'
-import cors from 'cors'
 import bodyParser from 'body-parser'
 
 export const configMiddlewares = (app: INestApplication) => {
   app.use(serveFavicon(path.join(process.cwd(), 'public/assets/favicon.ico')))
-  app.use(cors())
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
 }
