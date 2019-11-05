92ee611a750e05a1e2d5164d56f7835d4125c746
YanceyOfficial <yanceyofficial@gmail.com>
2019-11-05 19:24:50 +0800

    :sparkles: feat: delete useless files

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,20 +1,7 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
-// const {
-//   DATABASE_HOST: HOST,
-//   DATABASE_PORT: PORT,
-//   DATABASE_COLLECTION: COLLECTION,
-//   DATABASE_USER: USER,
-//   DATABASE_PWD: PWD,
-// } = process.env
-
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      // `mongodb://${USER}:${PWD}@${HOST}:${PORT}/${COLLECTION}`,
-      'mongodb://localhost:27017/blog',
-    ),
-  ],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/blog')],
 })
 export class DataBaseModule {}
