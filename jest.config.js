dff3ef41babfe64d5a00bab6cbc43bc009da78e7
Yancey Leo <yanceyofficial@gmail.com>
2019-12-04 02:11:13 +0800

    :wrench: config: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,7 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: ['json', 'text', 'html'],
+  coverageReporters: ['text', 'lcov', 'json', 'clover'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: null,
