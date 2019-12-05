b9dda5b3d0930799ab669a621edd2d56ec65d387
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-05 14:14:17 +0800

    feat(.env.example): update .env.example

--- src/sms/sms.resolver.ts
+++ src/sms/sms.resolver.ts
@@ -1,4 +1,4 @@
-import { UseGuards } from '@nestjs/common'
+// import { UseGuards } from '@nestjs/common'
 import { Args, Query, Resolver, Mutation } from '@nestjs/graphql'
 import { SMSService } from './sms.service'
 import { SMSModel } from './models/sms.model'
@@ -6,7 +6,7 @@ import { SendSMSRes } from './dtos/sendSMSRes.dto'
 import { ValidateSMSRes } from './dtos/validateSMSRes.dto'
 import { SendSMSInput } from './dtos/sendSMS.input'
 import { ValidateSMSInput } from './dtos/validateSMS.input'
-import { GqlAuthGuard } from '../guard/gqlAuth.guard'
+// import { GqlAuthGuard } from '../guard/gqlAuth.guard'
 
 @Resolver(() => SMSModel)
 export class SMSResolver {
@@ -15,7 +15,7 @@ export class SMSResolver {
   }
 
   @Query(() => [SMSModel])
-  @UseGuards(GqlAuthGuard)
+  // @UseGuards(GqlAuthGuard)
   public getAllSMS(): Promise<SMSModel[]> {
     return this.smsService.getAll()
   }
