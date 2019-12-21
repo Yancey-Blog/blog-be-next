6eb0c4ad2267068941fd1cfd2a36087e7735c5af
Yancey Leo <yanceyofficial@gmail.com>
2019-12-21 23:33:35 +0800

    feat: clean dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
-// import { configMiddlewares } from './middlewares/index.middleware'
+import { configMiddlewares } from './middlewares/index.middleware'
 import { AppModule } from './app.module'
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter())
-  // configMiddlewares(app)
+  configMiddlewares(app)
   await app.listen(process.env.port || 3002, () => console.log('Server is listening on port 3002.'))
 }
 
--- src/middlewares/index.middleware.ts
+++ src/middlewares/index.middleware.ts
@@ -1,24 +1,22 @@
-import { INestApplication } from '@nestjs/common'
-import path from 'path'
-import serveFavicon from 'serve-favicon'
-import bodyParser from 'body-parser'
-import morgan from 'morgan'
-import helmet from 'helmet'
+import { NestFastifyApplication } from '@nestjs/platform-fastify'
+// import path from 'path'
+// import serveFavicon from 'serve-favicon'
+// import bodyParser from 'body-parser'
+// import morgan from 'morgan'
+// import helmet from 'fastify-helmet'
 // import csurf from 'csurf'
-// import rateLimit from 'express-rate-limit'
+import rateLimit from 'fastify-rate-limit'
 
-export const configMiddlewares = (app: INestApplication) => {
-  app.use(serveFavicon(path.join(process.cwd(), 'public/assets/favicon/favicon.ico')))
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(morgan('combined'))
-  app.use(helmet())
-  // app.use(csurf())
-  app.enableCors({})
-  // app.use(
-  //   rateLimit({
-  //     windowMs: 15 * 60 * 1000, // 15 minutes
-  //     max: 100, // limit each IP to 100 requests per windowMs
-  //   }),
-  // )
+export const configMiddlewares = (app: NestFastifyApplication) => {
+  // app.register(serveFavicon(path.join(process.cwd(), 'public/assets/favicon/favicon.ico')))
+  // app.register(bodyParser.json())
+  // app.register(bodyParser.urlencoded({ extended: true }))
+  // app.register(morgan('combined'))
+  // app.register(helmet())
+  // app.register(csurf())
+  // app.enableCors({})
+  app.register(rateLimit, {
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
 }
