394b8ac1fa8c0de500b980b5cbb44d13277029a4
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-26 15:03:12 +0800

    feat: add UpdateLiveTourInput

--- src/live-tours/dtos/update-open-source.input.ts
+++ src/live-tours/dtos/update-open-source.input.ts
@@ -1,9 +1,9 @@
 import { InputType, Field } from 'type-graphql'
 import { IsString, IsNotEmpty } from 'class-validator'
-import { CreateOpenSourceInput } from './create-open-source.input'
+import { CreateLiveTourInput } from './create-open-source.input'
 
 @InputType()
-export class UpdateOpenSourceInput extends CreateOpenSourceInput {
+export class UpdateLiveTourInput extends CreateLiveTourInput {
   @Field()
   @IsString()
   @IsNotEmpty()
