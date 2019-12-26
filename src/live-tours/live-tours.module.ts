411e83d25bc60e034e40e384a06b46efb79772fa
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-26 14:43:22 +0800

    feat: finish liveTours module

--- src/live-tours/live-tours.module.ts
+++ src/live-tours/live-tours.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { LiveToursSchema } from './schemas/live-tours.schema'
 import { LiveToursService } from './live-tours.service'
 import { LiveToursResolver } from './live-tours.resolver'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'LiveTour', schema: LiveToursSchema }])],
   providers: [LiveToursResolver, LiveToursService],
 })
 export class LiveToursModule {}
