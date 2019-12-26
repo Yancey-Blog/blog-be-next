dd5916b958a4b23dab2d9ca384bcea3fc0af8896
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-26 15:10:57 +0800

    feat: update mutation input

--- src/live-tours/dtos/update-live-tour.input.ts
+++ src/live-tours/dtos/update-live-tour.input.ts
@@ -1,6 +1,6 @@
 import { InputType, Field } from 'type-graphql'
 import { IsString, IsNotEmpty } from 'class-validator'
-import { CreateLiveTourInput } from './create-open-source.input'
+import { CreateLiveTourInput } from './create-live-tour.input'
 
 @InputType()
 export class UpdateLiveTourInput extends CreateLiveTourInput {
