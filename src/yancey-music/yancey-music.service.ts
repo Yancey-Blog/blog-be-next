2737f8af1ff183f4e8d3d1648bc8dd26c203ada6
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-26 15:56:07 +0800

    feat: update YanceyMusicService

--- src/yancey-music/yancey-music.service.ts
+++ src/yancey-music/yancey-music.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+import { CreateYanceyMusicInput } from './dtos/create-yancey-music.input'
+import { UpdateYanceyMusicInput } from './dtos/update-yancey-music.input'
+import { YanceyMusicModel } from './models/yancey-music.model'
+import { YanceyMusic } from './interfaces/yancey-music.interface'
+import { BatchDeleteModel } from '../database/models/database.model'
 
 @Injectable()
-export class YanceyMusicService {}
+export class YanceyMusicService {
+  constructor(
+    @InjectModel('YanceyMusic')
+    private readonly yanceyMusicModel: Model<YanceyMusic>,
+  ) {
+    this.yanceyMusicModel = yanceyMusicModel
+  }
+
+  public async findAll(): Promise<YanceyMusicModel[]> {
+    return this.yanceyMusicModel.find({}).sort({ updatedAt: -1 })
+  }
+
+  public async findOneById(id: string): Promise<YanceyMusicModel> {
+    return this.yanceyMusicModel.findById(id)
+  }
+
+  public async create(yanceyMusicInput: CreateYanceyMusicInput): Promise<YanceyMusicModel> {
+    return this.yanceyMusicModel.create(yanceyMusicInput)
+  }
+
+  public async update(yanceyMusicInput: UpdateYanceyMusicInput): Promise<YanceyMusicModel> {
+    const { id, ...rest } = yanceyMusicInput
+    return this.yanceyMusicModel.findByIdAndUpdate(id, rest, { new: true })
+  }
+
+  public async deleteOneById(id: string): Promise<YanceyMusicModel> {
+    return this.yanceyMusicModel.findByIdAndDelete(id)
+  }
+
+  public async batchDelete(ids: string[]): Promise<BatchDeleteModel> {
+    return this.yanceyMusicModel.deleteMany({
+      _id: { $in: ids },
+    })
+  }
+}
