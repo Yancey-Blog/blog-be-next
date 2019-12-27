1ccbd89c8d9dd2535e5308d50277bfbfbe6fc5d6
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-27 14:39:38 +0800

    feat: update PlayerModule

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { PlayerSchema } from './schemas/player.schema'
 import { PlayerResolver } from './player.resolver'
 import { PlayerService } from './player.service'
 
 @Module({
-  controllers: [PlayerResolver],
-  providers: [PlayerService],
+  imports: [MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }])],
+  providers: [PlayerResolver, PlayerService],
 })
 export class PlayerModule {}
