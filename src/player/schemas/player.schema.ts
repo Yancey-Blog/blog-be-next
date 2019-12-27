fc7c39f709d4bbb058d628efbdfb2e48fccfccbe
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-27 14:17:26 +0800

    feat: define PlayerSchema

---
+++ src/player/schemas/player.schema.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+import { v4 } from 'uuid'
+
+export const PlayerSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: v4,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    artist: {
+      type: String,
+      required: true,
+    },
+    lrc: {
+      type: String,
+      required: true,
+    },
+    coverUrl: {
+      type: String,
+      required: true,
+    },
+    musicFileUrl: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: 'player',
+    timestamps: true,
+  },
+)
