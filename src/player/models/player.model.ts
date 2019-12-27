83edf6b3434635a49d1f5b524115270c8a3b8dd4
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-27 14:20:37 +0800

    feat: define PlayerModel

---
+++ src/player/models/player.model.ts
@@ -0,0 +1,28 @@
+import { Field, ID, ObjectType } from 'type-graphql'
+
+@ObjectType()
+export class PlayerModel {
+  @Field(() => ID)
+  public readonly _id: string
+
+  @Field()
+  public readonly title: string
+
+  @Field()
+  public readonly artist: string
+
+  @Field()
+  public readonly lrc: string
+
+  @Field()
+  public readonly coverUrl: string
+
+  @Field()
+  public readonly musicFileUrl: string
+
+  @Field()
+  public readonly createdAt: string
+
+  @Field()
+  public readonly updatedAt: string
+}
