1e3d56d77e0356eac4e80ee45078fc992a0d98fc
Yancey Leo <yanceyofficial@gmail.com>
2020-02-03 07:41:37 +0800

    feat: upadte AgendaResolver

--- src/agenda/agenda.resolver.ts
+++ src/agenda/agenda.resolver.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common'
+import { UseGuards } from '@nestjs/common'
+import { Args, Query, Resolver, Mutation } from '@nestjs/graphql'
+import { ID } from 'type-graphql'
+import { AgendaService } from './agenda.service'
+import { AgendaModel } from './models/agenda.model'
+import { CreateAgendaInput } from './dtos/create-agenda.input'
+import { UpdateAgendaInput } from './dtos/update-agenda.input'
+import { BatchDeleteModel } from '../database/models/batch-delete.model'
+import { GqlAuthGuard } from '../guard/gqlAuth.guard'
 
-@Controller('agenda')
-export class AgendaController {}
+@Resolver(() => AgendaModel)
+export class AgendaResolver {
+  constructor(private readonly agendaService: AgendaService) {
+    this.agendaService = agendaService
+  }
+
+  @Query(() => [AgendaModel])
+  public async getAgenda(): Promise<AgendaModel[]> {
+    return this.agendaService.findAll()
+  }
+
+  @Mutation(() => AgendaModel)
+  @UseGuards(GqlAuthGuard)
+  public async createAgenda(@Args('input') input: CreateAgendaInput): Promise<AgendaModel> {
+    return this.agendaService.create(input)
+  }
+
+  @Mutation(() => AgendaModel)
+  @UseGuards(GqlAuthGuard)
+  public async updateAgendaById(@Args('input') input: UpdateAgendaInput): Promise<AgendaModel> {
+    return this.agendaService.update(input)
+  }
+
+  @Mutation(() => AgendaModel)
+  @UseGuards(GqlAuthGuard)
+  public async deleteAgendaById(
+    @Args({ name: 'id', type: () => ID }) id: string,
+  ): Promise<AgendaModel> {
+    return this.agendaService.deleteOneById(id)
+  }
+}
