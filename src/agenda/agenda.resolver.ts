95deb473109fb8e06d0e84330e232b0eef6dc1a1
Yancey Leo <yanceyofficial@gmail.com>
2020-02-03 08:22:15 +0800

    feat: update AgendaResolver

--- src/agenda/agenda.resolver.ts
+++ src/agenda/agenda.resolver.ts
@@ -20,19 +20,19 @@ export class AgendaResolver {
   }
 
   @Mutation(() => AgendaModel)
-  // @UseGuards(GqlAuthGuard)
+  @UseGuards(GqlAuthGuard)
   public async createAgenda(@Args('input') input: CreateAgendaInput) {
     return this.agendaService.create(input)
   }
 
   @Mutation(() => AgendaModel)
-  // @UseGuards(GqlAuthGuard)
+  @UseGuards(GqlAuthGuard)
   public async updateAgendaById(@Args('input') input: UpdateAgendaInput) {
     return this.agendaService.update(input)
   }
 
   @Mutation(() => AgendaModel)
-  // @UseGuards(GqlAuthGuard)
+  @UseGuards(GqlAuthGuard)
   public async deleteAgendaById(@Args({ name: 'id', type: () => ID }) id: string) {
     return this.agendaService.deleteOneById(id)
   }
