179f4cef9794c1b4769e85775bfca6b25fcfdbd9
Yancey Leo <yanceyofficial@gmail.com>
2020-03-28 21:49:28 +0800

    feat: update PlayerResolver

--- src/player/player.resolver.ts
+++ src/player/player.resolver.ts
@@ -1,3 +1,4 @@
+import { UseGuards } from '@nestjs/common'
 import { Args, Query, Resolver, Mutation, ID } from '@nestjs/graphql'
 import { PlayerService } from './player.service'
 import { PlayerModel } from './models/player.model'
@@ -5,6 +6,7 @@ import { BatchDeleteModel } from '../database/models/batch-delete.model'
 import { BatchUpdateModel } from '../database/models/batch-update.model'
 import { CreatePlayerInput } from './dtos/create-player.input'
 import { UpdatePlayerInput } from './dtos/update-player.input'
+import { GqlAuthGuard } from '../guard/gqlAuth.guard'
 
 @Resolver(() => PlayerModel)
 export class PlayerResolver {
@@ -23,26 +25,31 @@ export class PlayerResolver {
   }
 
   @Mutation(() => PlayerModel)
+  @UseGuards(GqlAuthGuard)
   public async createPlayer(@Args('input') input: CreatePlayerInput) {
     return this.playerService.create(input)
   }
 
   @Mutation(() => PlayerModel)
+  @UseGuards(GqlAuthGuard)
   public async updatePlayerById(@Args('input') input: UpdatePlayerInput) {
     return this.playerService.update(input)
   }
 
   @Mutation(() => PlayerModel)
+  @UseGuards(GqlAuthGuard)
   public async deletePlayerById(@Args({ name: 'id', type: () => ID }) id: string) {
     return this.playerService.deleteOneById(id)
   }
 
   @Mutation(() => BatchDeleteModel)
+  @UseGuards(GqlAuthGuard)
   public async deletePlayers(@Args({ name: 'ids', type: () => [ID] }) ids: string[]) {
     return this.playerService.batchDelete(ids)
   }
 
   @Mutation(() => BatchUpdateModel)
+  @UseGuards(GqlAuthGuard)
   public async offlinePlayers(@Args({ name: 'ids', type: () => [ID] }) ids: string[]) {
     return this.playerService.batchUpdate(ids)
   }
