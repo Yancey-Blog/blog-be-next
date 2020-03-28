23b19777dd18549c07ebd19283d3b826c0975ce5
Yancey Leo <yanceyofficial@gmail.com>
2020-03-28 21:51:45 +0800

    feat: update BestAlbumsResolver

--- src/best-albums/best-albums.resolver.ts
+++ src/best-albums/best-albums.resolver.ts
@@ -1,9 +1,11 @@
+import { UseGuards } from '@nestjs/common'
 import { Args, Query, Resolver, Mutation, ID } from '@nestjs/graphql'
 import { BestAlbumsService } from './best-albums.service'
 import { BestAlbumModel } from './models/best-albums.model'
 import { BatchDeleteModel } from '../database/models/batch-delete.model'
 import { CreateBestAlbumInput } from './dtos/create-best-album.input'
 import { UpdateBestAlbumInput } from './dtos/update-best-album.input'
+import { GqlAuthGuard } from '../guard/gqlAuth.guard'
 
 @Resolver(() => BestAlbumModel)
 export class BestAlbumsResolver {
@@ -22,21 +24,25 @@ export class BestAlbumsResolver {
   }
 
   @Mutation(() => BestAlbumModel)
+  @UseGuards(GqlAuthGuard)
   public async createBestAlbum(@Args('input') input: CreateBestAlbumInput) {
     return this.bestAlbumsService.create(input)
   }
 
   @Mutation(() => BestAlbumModel)
+  @UseGuards(GqlAuthGuard)
   public async updateBestAlbumById(@Args('input') input: UpdateBestAlbumInput) {
     return this.bestAlbumsService.update(input)
   }
 
   @Mutation(() => BestAlbumModel)
+  @UseGuards(GqlAuthGuard)
   public async deleteBestAlbumById(@Args({ name: 'id', type: () => ID }) id: string) {
     return this.bestAlbumsService.deleteOneById(id)
   }
 
   @Mutation(() => BatchDeleteModel)
+  @UseGuards(GqlAuthGuard)
   public async deleteBestAlbums(@Args({ name: 'ids', type: () => [ID] }) ids: string[]) {
     return this.bestAlbumsService.batchDelete(ids)
   }
