0949572cc4326273ec556e8f564ac9ad1e2fbc5e
Yancey Leo <yanceyofficial@gmail.com>
2020-03-28 21:48:18 +0800

    feat: update YanceyMusicResolver

--- src/yancey-music/yancey-music.resolver.ts
+++ src/yancey-music/yancey-music.resolver.ts
@@ -1,9 +1,11 @@
+import { UseGuards } from '@nestjs/common'
 import { Args, Query, Resolver, Mutation, ID } from '@nestjs/graphql'
 import { YanceyMusicService } from './yancey-music.service'
 import { YanceyMusicModel } from './models/yancey-music.model'
 import { BatchDeleteModel } from '../database/models/batch-delete.model'
 import { CreateYanceyMusicInput } from './dtos/create-yancey-music.input'
 import { UpdateYanceyMusicInput } from './dtos/update-yancey-music.input'
+import { GqlAuthGuard } from '../guard/gqlAuth.guard'
 
 @Resolver(() => YanceyMusicModel)
 export class YanceyMusicResolver {
@@ -22,21 +24,25 @@ export class YanceyMusicResolver {
   }
 
   @Mutation(() => YanceyMusicModel)
+  @UseGuards(GqlAuthGuard)
   public async createYanceyMusic(@Args('input') input: CreateYanceyMusicInput) {
     return this.yanceyMusicsService.create(input)
   }
 
   @Mutation(() => YanceyMusicModel)
+  @UseGuards(GqlAuthGuard)
   public async updateYanceyMusicById(@Args('input') input: UpdateYanceyMusicInput) {
     return this.yanceyMusicsService.update(input)
   }
 
   @Mutation(() => YanceyMusicModel)
+  @UseGuards(GqlAuthGuard)
   public async deleteYanceyMusicById(@Args({ name: 'id', type: () => ID }) id: string) {
     return this.yanceyMusicsService.deleteOneById(id)
   }
 
   @Mutation(() => BatchDeleteModel)
+  @UseGuards(GqlAuthGuard)
   public async deleteYanceyMusic(@Args({ name: 'ids', type: () => [ID] }) ids: string[]) {
     return this.yanceyMusicsService.batchDelete(ids)
   }
