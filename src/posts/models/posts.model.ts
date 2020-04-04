ec53d27cefc43fa031a3e15ad5d28141bfeb84a4
Yancey Leo <yanceyofficial@gmail.com>
2020-04-04 23:26:05 +0800

    feat: define PostModel

---
+++ src/posts/models/posts.model.ts
@@ -0,0 +1,40 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql'
+
+@ObjectType()
+export class PostModel {
+  @Field(() => ID)
+  public readonly _id: string
+
+  @Field()
+  public readonly posterUrl: string
+
+  @Field()
+  public readonly title: string
+
+  @Field()
+  public readonly summary: string
+
+  @Field()
+  public readonly content: string
+
+  @Field(() => [String])
+  public readonly tags: string[]
+
+  @Field()
+  public readonly lastModifiedDate: Date
+
+  @Field()
+  public readonly like: number
+
+  @Field()
+  public readonly pv: number
+
+  @Field()
+  public readonly isPublic: boolean
+
+  @Field()
+  public readonly createdAt: Date
+
+  @Field()
+  public readonly updatedAt: Date
+}
