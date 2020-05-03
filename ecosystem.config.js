aca7abf67aba2fa28a26b0c3318b2a2ea10dc796
Yancey Leo <yanceyofficial@gmail.com>
2020-05-03 16:36:59 +0800

    build: add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: 'blog-be',
+      script: './dist/main.js',
+      watch: true,
+      ignore_watch: ['logs'],
+      autorestart: true,
+      max_memory_restart: '1G',
+      instances: 2,
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  // deploy: {
+  //   production: {
+  //     user: 'SSH_USERNAME',
+  //     host: 'SSH_HOSTMACHINE',
+  //     ref: 'origin/master',
+  //     repo: 'https://github.com/Yancey-Blog/blog-be-next',
+  //     path: 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': '',
+  //   },
+  // },
+}
