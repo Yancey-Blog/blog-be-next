2393a22f192df47abf90444ae44302ac99fa28b3
Yancey Leo <yanceyofficial@gmail.com>
2020-05-04 15:58:46 +0800

    build: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,17 +14,4 @@ module.exports = {
       },
     },
   ],
-
-  // deploy: {
-  //   production: {
-  //     user: 'SSH_USERNAME',
-  //     host: 'SSH_HOSTMACHINE',
-  //     ref: 'origin/master',
-  //     repo: 'https://github.com/Yancey-Blog/blog-be-next',
-  //     path: 'DESTINATION_PATH',
-  //     'pre-deploy-local': '',
-  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-  //     'pre-setup': '',
-  //   },
-  // },
 }
